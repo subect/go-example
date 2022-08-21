563cd0357218790eae3fbd185b2457e4d9fe59ec
huxiang <1901608396@qq.com>
2022-08-21 15:23:31 +0800

    feat(go-example): 常量的定义

--- yf/exam_1.go
+++ yf/exam_1.go
@@ -1,8 +1,19 @@
 package main
 
-import _ "./in"
+import (
+	_ "./in"
+	"fmt"
+)
+
+const (
+	n1 = iota + 1
+	n2
+	n3
+	n4
+	n5
+	n6
+)
 
 func main() {
-	//i := 65
-	//fmt.Println(string(i))
+	fmt.Println(n1, n2, n3, n4, n5, n6)
 }
